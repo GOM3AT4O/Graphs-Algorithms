00c2d78fbfa57d2677584c2bfb26ecb7403dca5f
Mohamed Gomaa Abdel Zaher Abdel Halim Omar <mohamedgomaa44118@gmail.com>
2026-05-18 08:12:09 +0300

    Add files via upload

---
+++ DSA3/datalayer/Edge.java
@@ -0,0 +1,8 @@
+public record Edge(int u , int v , int weight) implements Comparable<Edge> {
+
+    @Override
+    public int compareTo(Edge o){
+        return Integer.compare(this.weight , o.weight);
+    }
+
+}
