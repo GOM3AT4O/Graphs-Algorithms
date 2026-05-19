628ddaa43a3d217338371f50311fa77889ce5498
Mohamed Gomaa Abdel Zaher Abdel Halim Omar <mohamedgomaa44118@gmail.com>
2026-05-19 10:30:54 +0300

    Delete DSA3/datalayer directory

--- DSA3/datalayer/Edge.java
+++
@@ -1,8 +0,0 @@
-public record Edge(int u , int v , int weight) implements Comparable<Edge> {
-
-    @Override
-    public int compareTo(Edge o){
-        return Integer.compare(this.weight , o.weight);
-    }
-
-}
